61ca0eb3d2f7ed09945f32d0c58ef2c829ef4462
Alan Wang <wzh19921016@gmail.com>
2025-02-06 21:20:43 -0800

    exercise 5B

--- src/app/page.js
+++ src/app/page.js
@@ -4,26 +4,29 @@ import BlogSummaryCard from "@/components/BlogSummaryCard";
 
 import styles from "./homepage.module.css";
 import { getBlogPostList } from "@/helpers/file-helpers";
+import { MotionConfig } from "framer-motion";
 
 async function Home() {
   const blogposts = await getBlogPostList();
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.mainHeading}>Latest Content:</h1>
+    <MotionConfig reducedMotion="user">
+      <div className={styles.wrapper}>
+        <h1 className={styles.mainHeading}>Latest Content:</h1>
 
-      {
-        /* TODO: Iterate over the data read from the file system! */
-        blogposts.map((blog) => (
-          <BlogSummaryCard
-            slug={blog.slug}
-            title={blog.title}
-            abstract={blog.abstract}
-            publishedOn={blog.publishedOn}
-            key={blog.slug}
-          />
-        ))
-      }
-    </div>
+        {
+          /* TODO: Iterate over the data read from the file system! */
+          blogposts.map((blog) => (
+            <BlogSummaryCard
+              slug={blog.slug}
+              title={blog.title}
+              abstract={blog.abstract}
+              publishedOn={blog.publishedOn}
+              key={blog.slug}
+            />
+          ))
+        }
+      </div>
+    </MotionConfig>
   );
 }
 
