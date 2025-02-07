37d4354150c7b76c02ce36edb0659a95cff7664e
Alan Wang <wzh19921016@gmail.com>
2025-02-07 12:52:54 -0800

    exercise 7

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Work_Sans, Spline_Sans_Mono } from "next/font/google";
 import clsx from "clsx";
+import { cookies } from "next/headers";
 
 import { LIGHT_TOKENS, DARK_TOKENS, BLOG_TITLE } from "@/constants";
 
@@ -22,8 +23,8 @@ const monoFont = Spline_Sans_Mono({
 });
 
 function RootLayout({ children }) {
-  // TODO: Dynamic theme depending on user preference
-  const theme = "light";
+  const savedTheme = cookies().get('color-theme');
+  const theme = savedTheme?.value || "light";
 
   return (
     <html
@@ -33,7 +34,7 @@ function RootLayout({ children }) {
       style={theme === "light" ? LIGHT_TOKENS : DARK_TOKENS}
     >
       <body>
-        <Header theme={theme} />
+        <Header initialTheme={theme} />
         <main>{children}</main>
         <Footer />
       </body>
